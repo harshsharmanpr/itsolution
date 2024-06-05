c6e37ab5f70a247f7953c3b7f46ae3b203d4f982
rajoo yadav <yadavrajoo578@gmail.com>
2024-06-05 17:44:20 +0530

    footer

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../footer/Footer";
 import BokkCall from "../bookCall/BokkCall";
+import SoftwareHomePage from "../SoftwareHomePage/SoftwareHomePage";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <SoftwareHomePage></SoftwareHomePage>
       <BokkCall />
       <Footer />
     </div>
